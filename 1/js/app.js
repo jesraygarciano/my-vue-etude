112b71bf5aeb6a21b479181a5f0c4739a3869dc3
Jesray Garciano <jesray.garciano@gmail.com>
2018-06-14 13:33:24 +0800

    Adding navigation and Continued exercise 1

--- 1/js/app.js
+++ 1/js/app.js
@@ -9,7 +9,28 @@ var app = new Vue({
         link: 'https://github.com/jesraygarciano',
         targetBlank: '_blank',
         inStock: 8,
-        onSale: true
+        onSale: true,
+        details: [
+            "80% cotton",
+            "20% polyester",
+            "Gender-neutral"
+        ],
+        variants: [
+            {
+                variantId: 2212,
+                variantColor: "pusia"
+            },
+            {
+                variantId: 2213,
+                variantColor: "yellowpink"
+            }
+        ],
+        sockSizes: [
+            "Small",
+            "Medium",
+            "Large",
+            "Extra Large"
+        ]
     }
 
 });
