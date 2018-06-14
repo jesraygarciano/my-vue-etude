07b1ddd6c8c195d27410be336c4b09107a393d88
Jesray Garciano <jesray.garciano@gmail.com>
2018-06-14 10:49:30 +0800

    Initial commit and Exer#1

--- 1/js/app.js
+++ 1/js/app.js
@@ -1,1 +1,14 @@
-var product = "Socks";
+var app = new Vue({
+
+    el: '#app',
+    data: {
+        product: 'Medyas',
+        description: 'Fuzzy Medyas',
+        image: 'image/vm-socks-blue.png',
+        altText: 'Pair of Socks'
+    }
+
+});
+
+
+// var product = "Socks";
