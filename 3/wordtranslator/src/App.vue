90d653684e370e7c93292959f4248a4c44737ae9
Jesray Garciano <jesray.garciano@gmail.com>
2018-06-22 11:46:37 +0800

    Continued exercise #3 the word translator using API

--- 3/wordtranslator/src/App.vue
+++ 3/wordtranslator/src/App.vue
@@ -1,13 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
+    <h1>Word Translate</h1>
+    <h5>Powered by Vue.js</h5>
+    <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
   </div>
 </template>
 
 <script>
+import TranslateForm from './components/TranslateForm'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    TranslateForm
+  },
+  methods: {
+    translateText: function(text){
+
+    }
+  }
 }
 </script>
 
---
+++ 3/wordtranslator/src/components/TranslateForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="translateform">
+      Translate Form
+      <form v-on:submit="formSubmit">
+          <input type="text" v-model="textToTranslate" placeholder="Enter a Word...">
+          <input type="submit" value="Translate">
+      </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'translateform',
+  data(){
+      return {
+          textToTranslate: ''
+      }
+  },
+  methods: {
+      formSubmit(e){
+        //   alert('Submitted');
+        this.$emit('formSubmit', this.textToTranslate);
+          e.preventDefault();
+          
+      }
+  }
+}
+</script>
+
+<style>
+</style>
