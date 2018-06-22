b277fcb123b9deb22b6e176804f510fe85f71443
Jesray Garciano <jesray.garciano@gmail.com>
2018-06-22 14:52:59 +0800

    Adding components

--- 3/wordtranslator/src/App.vue
+++ 3/wordtranslator/src/App.vue
@@ -5,22 +5,31 @@
     <h1>Word Translate</h1>
     <h5>Powered by Vue.js</h5>
     <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+    <TranslateOutput v-text="translateText"></TranslateOutput>
   </div>
 </template>
 
 <script>
 import TranslateForm from './components/TranslateForm'
+import Translateoutput from './components/TranslateOutput'
 
 export default {
   name: 'App',
   components: {
-    TranslateForm
+    TranslateForm,
+    TranslateOutput
+  },
+  data: function(){
+    return{
+      translateText:''
+    }
   },
   methods: {
     translateText: function(text){
-      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180622T051557Z.84acc5bb4f9921ad.5bc0acb6037cb072100184329f20151a72d9d79e&lang=ru&text='+text)
+      this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20180622T051557Z.84acc5bb4f9921ad.5bc0acb6037cb072100184329f20151a72d9d79e&lang=es&text='+text)
       .then((response) => {
-        console.log(response);
+        this.translateText= response.body.text[0];
+
       });
     }
   }
---
+++ 3/wordtranslator/src/components/TranslateOutput.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="translateoutput">
+        <h2>{{ translatedText }}</h2>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'translateOutput',
+    props: ['translatedText']
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
